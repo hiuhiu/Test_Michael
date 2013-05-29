f6dbd95271dc68cd357764be0e7c5d4e39c7c4bd
Michael Bortlik <Michael.Bortlik@gmx.de>
2013-05-29 11:13:04 +0200

    function add implemented

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,22 @@
 public class Main {
 
 	/**
-	 * @param args
+	 * @param args asdf
 	 */
+	
+	int blub1;
+	String blub2;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public static int add(int a, int b)
+	{
+	
+		return a + b;
+		
 	}
 
 }
