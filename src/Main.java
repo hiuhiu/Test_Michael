d886c9e7e05a10b4bdc3bc0927ca146b2706a747
Michael Bortlik <Michael.Bortlik@gmx.de>
2013-05-29 11:22:23 +0200

    function multi implemented

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,12 @@ public class Main {
 	int blub1;
 	String blub2;
 	
+	public static int multi(int a, int b)
+	{
+		
+		return a * b;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
