d6208619a6286211b0dfdba1358118013aaf9721
Michael Bortlik <Michael.Bortlik@gmx.de>
2013-05-29 11:29:12 +0200

    function div und variablen

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,22 @@
 public class Main {
 
 	/**
-	 * @param args asdf
+	 * @param args asdf huhoh oiahos
 	 */
 	
 	int blub1;
 	String blub2;
+	String michael;
 	
 	public static int multi(int a, int b)
 	{
 		
 		return a * b;
+	}
+	
+	public static double div(double a, double b)
+	{
+		return a / b;
 	}
 	
 	public static void main(String[] args) {
